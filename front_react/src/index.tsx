e8d7a5c6416727dd38c0c8b866eb6368714c92c9
Zapishnyi <zapishny@ukr.net>
2024-12-05 18:24:24 +0200

    react app on initial state

---
+++ front_react/src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { routerConfig } from './router/routerConfig';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  // <Provider store={store}>
+  <RouterProvider router={routerConfig} future={{ v7_startTransition: true }} />,
+  // </Provider>,
+);
+
