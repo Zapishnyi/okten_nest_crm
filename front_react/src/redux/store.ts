bce8598dba155bd9a4ed616206216e6f17b50cd0
Zapishnyi <zapishny@ukr.net>
2024-12-11 20:11:08 +0200

    Unused slice removed

--- front_react/src/redux/store.ts
+++ front_react/src/redux/store.ts
@@ -1,12 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { querySlice } from './Slices/querySlice';
 import { ordersSlice } from './Slices/ordersSlice';
 import { paginationSlice } from './Slices/paginationSlice';
 
 export const store = configureStore({
   reducer: {
-    orderQuery: querySlice.reducer,
     orders: ordersSlice.reducer,
     pagination: paginationSlice.reducer,
   },
