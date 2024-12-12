787c181bbc39fe2b8f0e3f3eb57ad6009004eeef
Zapishnyi <zapishny@ukr.net>
2024-12-12 22:39:24 +0200

    transaction level implementation re-arranged

--- back_nest/src/modules/order/order.module.ts
+++ back_nest/src/modules/order/order.module.ts
@@ -5,9 +5,10 @@ import { OrderController } from './order.controller';
 import { OrderService } from './services/order.service';
 import { OrderPresenterService } from './services/order-presenter.service';
 import { AuthModule } from '../auth/auth.module';
+import { IsolationLevelModule } from '../transaction-isolation-level/isolation-level.module';
 
 @Module({
-  imports: [RepositoryModule, AuthModule],
+  imports: [RepositoryModule, AuthModule, IsolationLevelModule],
   controllers: [OrderController],
   providers: [OrderService, OrderPresenterService],
 })
--- back_nest/src/modules/user/user.module.ts
+++ back_nest/src/modules/user/user.module.ts
@@ -5,9 +5,14 @@ import { AuthModule } from '../auth/auth.module';
 import { RepositoryModule } from '../repository/repository.module';
 import { UserService } from './services/user.service';
 import { AdminService } from './services/admin.service';
+import { IsolationLevelModule } from '../transaction-isolation-level/isolation-level.module';
 
 @Module({
-  imports: [forwardRef(() => AuthModule), RepositoryModule],
+  imports: [
+    forwardRef(() => AuthModule),
+    RepositoryModule,
+    IsolationLevelModule,
+  ],
   controllers: [AdminController],
   providers: [UserPresenterService, UserService, AdminService],
   exports: [UserPresenterService, UserService, AdminService],
