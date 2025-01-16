2343b65d2a15b7af76e966fd15ca0b5d6b014620
Zapishnyi <zapishny@ukr.net>
2025-01-16 14:20:34 +0200

    health order corrected

--- back_nest/src/modules/health/health.controller.ts
+++ back_nest/src/modules/health/health.controller.ts
@@ -3,7 +3,7 @@ import { ApiTags } from '@nestjs/swagger';
 
 import { HealthResDto } from './res/health.res.dto';
 
-@ApiTags('8.Health')
+@ApiTags('5.Health')
 @Controller('health')
 export class HealthController {
   @Get()
