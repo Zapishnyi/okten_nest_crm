6c17232af3388dfe2c1d2d427d270c34ac826573
Zapishnyi <zapishny@ukr.net>
2025-01-25 10:43:30 +0200

    linter settings udjusted

--- front_react/eslint.config.mjs
+++ front_react/eslint.config.mjs
@@ -1,14 +1,15 @@
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
-import _import from 'eslint-plugin-import';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import _import from "eslint-plugin-import";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,58 +19,74 @@ const compat = new FlatCompat({
   allConfig: js.configs.all,
 });
 
-export default [...fixupConfigRules(compat.extends(
-  'eslint:recommended',
-  'plugin:react/recommended',
-  'plugin:react-hooks/recommended',
-  'plugin:@typescript-eslint/recommended',
-)), {
-  plugins: {
-    import: fixupPluginRules(_import),
-    react: fixupPluginRules(react),
-    'react-hooks': fixupPluginRules(reactHooks),
-    '@typescript-eslint': fixupPluginRules(typescriptEslint),
-  },
-
-  languageOptions: {
-    globals: {
-      ...globals.browser,
+export default [
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:@typescript-eslint/recommended"
+    )
+  ),
+  {
+    plugins: {
+      import: fixupPluginRules(_import),
+      react: fixupPluginRules(react),
+      "react-hooks": fixupPluginRules(reactHooks),
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
     },
 
-    parser: tsParser,
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
 
-    parserOptions: {
-      project: './tsconfig.json',
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
 
-      ecmaFeatures: {
-        jsx: true,
+      parserOptions: {
+        project: "./tsconfig.json",
+
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
+
+    rules: {
+      "react/prop-types": "off",
+      "react/display-name": "off",
+      "react/react-in-jsx-scope": "off",
+
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "external",
+              position: "before",
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ["react"],
+          "newlines-between": "always",
+
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
   },
-
-  rules: {
-    'react/prop-types': 'off',
-    'react/display-name': 'off',
-    'react/react-in-jsx-scope': 'off',
-
-    'import/order': ['error', {
-      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-
-      pathGroups: [{
-        pattern: 'react',
-        group: 'external',
-        position: 'before',
-      }],
-
-      pathGroupsExcludedImportTypes: ['react'],
-      'newlines-between': 'always',
-
-      alphabetize: {
-        order: 'asc',
-        caseInsensitive: true,
-      },
-    }],
-  },
-}];
+];
--- front_react/src/pages/Orders/Orders.tsx
+++ front_react/src/pages/Orders/Orders.tsx
@@ -1,48 +1,62 @@
-import React, { FC, memo, useEffect } from 'react';
+import React, { FC, memo, useEffect } from "react";
 
-import { useLocation, useSearchParams } from 'react-router-dom';
+import { useLocation, useSearchParams } from "react-router-dom";
 
-import Pagination from '../../components/Pagination/Pagination';
-import Table from '../../components/Table/Table';
-import { initialOrdersQuery } from '../../constants/initialOrdersQuery';
-import { orderToReduced } from '../../helpers/order-to-reduced';
-import { queryToSearchParams } from '../../helpers/query-to-search-params-obj';
-import IOrderReduced from '../../interfaces/IOrderReduced';
-import { OrdersActions } from '../../redux/Slices/ordersSlice';
-import { useAppDispatch, useAppSelector } from '../../redux/store';
+import Pagination from "../../components/Pagination/Pagination";
+import Table from "../../components/Table/Table";
+import { initialOrdersQuery } from "../../constants/initialOrdersQuery";
+import { orderToReduced } from "../../helpers/order-to-reduced";
+import { queryToSearchParams } from "../../helpers/query-to-search-params-obj";
+import IOrderReduced from "../../interfaces/IOrderReduced";
+import { OrdersActions } from "../../redux/Slices/ordersSlice";
+import { useAppDispatch, useAppSelector } from "../../redux/store";
 
-import styles from './Orders.module.css';
-
+import styles from "./Orders.module.css";
 
 const Orders: FC = memo(() => {
-
   const location = useLocation();
 
-  const { orders, ordersLoadingState } = useAppSelector((state) => state.orders);
-  const { userLogged } = useAppSelector(state => state.users);
-  const { pages, page } = useAppSelector((state) => state.pagination.paginationData);
+  const { orders, ordersLoadingState } = useAppSelector(
+    (state) => state.orders
+  );
+  const { userLogged } = useAppSelector((state) => state.users);
+  const { pages, page } = useAppSelector(
+    (state) => state.pagination.paginationData
+  );
   const dispatch = useAppDispatch();
-  const [query, setQuery] = useSearchParams(queryToSearchParams(initialOrdersQuery));
+  const [query, setQuery] = useSearchParams(
+    queryToSearchParams(initialOrdersQuery)
+  );
 
   useEffect(() => {
-    if (!location.search.match(/^(?=.*\bsort=\b)(?=.*\bpage=\b)(?=.*\bsortBy=\b).*/)) {
+    if (
+      !location.search.match(
+        /^(?=.*\bsort=\b)(?=.*\bpage=\b)(?=.*\bsortBy=\b).*/
+      )
+    ) {
       setQuery(queryToSearchParams(initialOrdersQuery), { replace: true });
-      dispatch(OrdersActions.searchForOrders(queryToSearchParams(initialOrdersQuery)));
+      dispatch(
+        OrdersActions.searchForOrders(queryToSearchParams(initialOrdersQuery))
+      );
     } else {
-      dispatch(OrdersActions.searchForOrders(Object.fromEntries(query.entries())));
+      dispatch(
+        OrdersActions.searchForOrders(Object.fromEntries(query.entries()))
+      );
     }
-
   }, [location.search]);
   return (
     <div className={styles.wrapper}>
-      {!!orders.length &&
+      {!!orders.length && (
         <div className={styles.table_container}>
-          <Table<IOrderReduced> items={orders.map(order => orderToReduced(order))} />
+          <Table<IOrderReduced>
+            items={orders.map((order) => orderToReduced(order))}
+          />
           <Pagination page={page} pages={pages} />
         </div>
-      }
-      {!orders.length && !ordersLoadingState && userLogged &&
-        <h2 className={styles.no_data}>No data matches your request.</h2>}
+      )}
+      {!orders.length && !ordersLoadingState && userLogged && (
+        <h2 className={styles.no_data}>No data matches your request.</h2>
+      )}
     </div>
   );
 });
